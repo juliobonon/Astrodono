f84421d697aae41a8d4f4aa6a911d5db3cf23548
sarri <luan@pop-os.localdomain>
2020-03-10 09:14:47 -0300

    Add a image to navigation bottom bar

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_country_picker/FlutterCountryPickerPlugin.h>)
+#import <flutter_country_picker/FlutterCountryPickerPlugin.h>
+#else
+@import flutter_country_picker;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterCountryPickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterCountryPickerPlugin"]];
+}
+
+@end
